d89dbc7d2fd026178a37966e1c7338097306639e
nipun-dezoysa <nipunavishka123@gmail.com>
2025-03-11 23:52:21 +0530

    move InfoCard widget to shared folder

--- lib/screens/description/description.dart
+++ lib/screens/description/description.dart
@@ -1,20 +1,5 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DescriptionScreen(),
-    );
-  }
-}
+import 'package:torva/widgets/info_card.dart';
 
 class DescriptionScreen extends StatelessWidget {
   const DescriptionScreen({super.key});
@@ -58,9 +43,12 @@ class DescriptionScreen extends StatelessWidget {
                 height: 100,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(
-                      10), // Adjust the radius for rounding
+                    10,
+                  ), // Adjust the radius for rounding
                   image: DecorationImage(
-                    image: AssetImage('assets/profile1.jpg'), // Change as needed
+                    image: AssetImage(
+                      'assets/profile1.jpg',
+                    ), // Change as needed
                     fit: BoxFit.cover, // Adjust how the image fits
                   ),
                 ),
@@ -68,47 +56,60 @@ class DescriptionScreen extends StatelessWidget {
               SizedBox(height: 10),
               Text(
                 'GOMAN Treasure',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
               ),
               Text('#GC48Q9K', style: TextStyle(color: Colors.grey)),
               SizedBox(height: 10),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: List.generate(
-                    5,
-                    (index) => Icon(Icons.local_fire_department,
-                        color: Color(0xFF7033FA))),
+                  5,
+                  (index) => Icon(
+                    Icons.local_fire_department,
+                    color: Color(0xFF7033FA),
+                  ),
+                ),
               ),
               SizedBox(height: 20),
               InfoCard(
-                  icon: Icons.person_outline,
-                  title: 'Placed by',
-                  value: 'Rave'),
+                icon: Icons.person_outline,
+                title: 'Placed by',
+                value: 'Rave',
+              ),
               SizedBox(height: 10),
               InfoCard(
-                  icon: Icons.calendar_today_outlined,
-                  title: 'Date',
-                  value: '2025-01-18'),
+                icon: Icons.calendar_today_outlined,
+                title: 'Date',
+                value: '2025-01-18',
+              ),
               SizedBox(height: 10),
               InfoCard(
-                  icon: Icons.location_on_outlined,
-                  title: 'Location',
-                  value: 'Pitipana, Homagama'),
+                icon: Icons.location_on_outlined,
+                title: 'Location',
+                value: 'Pitipana, Homagama',
+              ),
               SizedBox(height: 10),
               InfoCard(
-                  icon: Icons.lightbulb_outline,
-                  title: 'Hint',
-                  value: 'Lorem ipsum dolor...'),
+                icon: Icons.lightbulb_outline,
+                title: 'Hint',
+                value: 'Lorem ipsum dolor...',
+              ),
               SizedBox(height: 10),
               InfoCard(
-                  icon: Icons.description_outlined,
-                  title: 'Description',
-                  value: 'Lorem ipsum dolor...'),
+                icon: Icons.description_outlined,
+                title: 'Description',
+                value: 'Lorem ipsum dolor...',
+              ),
               SizedBox(height: 10),
               InfoCard(
-                  icon: Icons.history_outlined,
-                  title: 'Past Logs',
-                  value: 'Found it on 2025-...'),
+                icon: Icons.history_outlined,
+                title: 'Past Logs',
+                value: 'Found it on 2025-...',
+              ),
               SizedBox(height: 20),
               Row(
                 children: [
@@ -120,23 +121,32 @@ class DescriptionScreen extends StatelessWidget {
                         borderRadius: BorderRadius.circular(50),
                       ),
                       padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 8),
+                        horizontal: 8,
+                        vertical: 8,
+                      ),
                       child: Row(
                         children: [
                           Expanded(
                             child: ElevatedButton(
                               onPressed: () => Navigator.pop(context),
                               style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    Color.fromARGB(255, 244, 243, 245),
+                                backgroundColor: Color.fromARGB(
+                                  255,
+                                  244,
+                                  243,
+                                  245,
+                                ),
                               ),
                               child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 16.0),
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 16.0,
+                                ),
                                 child: const Text(
                                   'Navigate',
                                   style: TextStyle(
-                                      color: Colors.black, fontSize: 20),
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                  ),
                                 ),
                               ),
                             ),
@@ -151,12 +161,15 @@ class DescriptionScreen extends StatelessWidget {
                                 backgroundColor: Color(0xFF7033FA),
                               ),
                               child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 16.0),
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 16.0,
+                                ),
                                 child: const Text(
                                   'Log Cache',
                                   style: TextStyle(
-                                      color: Colors.white, fontSize: 20),
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
                                 ),
                               ),
                             ),
@@ -170,30 +183,6 @@ class DescriptionScreen extends StatelessWidget {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class InfoCard extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final String value;
-
-  const InfoCard({super.key, required this.icon, required this.title, required this.value});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      child: ListTile(
-        leading: Icon(
-          icon,
-          color: Color(0xFF7033FA),
-          size: 30,
-        ),
-        title: Text(title, style: TextStyle(fontWeight: FontWeight.w500)),
-        trailing: Text(value, style: TextStyle(color: Colors.grey[700])),
       ),
     );
   }
